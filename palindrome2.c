8f4c935e5a17f9ca0efa4ceaa535a679fd9386a7
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-28 23:13:57 +0530

    version 83.0

--- palindrome2.c
+++ palindrome2.c
@@ -1,19 +1,27 @@
 #include<stdio.h>  
 int main()    
 {    
-int n,r,sum=0,temp;    
+int input,remainder,sum=0,store;    
 printf("enter the number=");    
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
+scanf("%d",&input);    
+store=input;    
+while(input!=0)    
 {    
-r=n%10;    
-sum=(sum*10)+r;    
-n=n/10;    
+remainder=input%10;    
+sum=(sum*10)+remainder; 
+//printf("%d",remainder);   
+input=input/10;   // it is just like we used i++ or i=i+1 
 }    
-if(temp==sum)    
+if(store==sum)    
 printf("palindrome number ");    
 else    
 printf("not palindrome");   
 return 0;  
 }   
+/*
+3456           345            34             3
+3456%10=6       5              4              3%10=3
+6               6=6x10+5=65    65x10+4=654    654x10+3=6543
+3456/10=345     345/10=34      34/10=3        3/10=0               
+now the loop is closed
+*/
